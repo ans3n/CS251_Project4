c70dfd419bc2e0483d09ea586fab66f5314c0a71
Ansen Tsan <139832422+ans3n@users.noreply.github.com>
2024-04-07 01:37:41 -0400

    Part 0 initial writing + github setup

--- src/graph.cpp
+++ src/graph.cpp
@@ -4,11 +4,65 @@
 using namespace cs251;
 
 void graph::read_edge_weights(const std::string& filePath) {
-	// TODO: Implement read edge weights	
-	throw std::logic_error("graph::" + std::string(__FUNCTION__) + "() not implemented");
+    std::ifstream file(filePath);                                               //check if ifstream is used or something else
+    if (!file.is_open()) {
+        return;
+    }
+
+    int numEdges = 0;
+    int numVertices = 0;
+    int source = 0;
+    int destination = 0;
+    int weight = 0;
+
+    file >> numVertices >> numEdges;    //reads the first two integers and stores
+    m_vertices.resize(numVertices);
+
+    //use num edges in case of directed
+    for (int i = 0; i < numEdges; i++) {
+        file >> source >> destination >> weight;
+        file.ignore();  //ignore new line
+
+        m_vertices[source].m_edges.push_back({weight, destination});    //only do one direction
+    }
+
+    file.close();
 }
 
 void graph::read_edge_colors(const std::string& filePath) {
-	// TODO: Implement read edge colors	
-	throw std::logic_error("graph::" + std::string(__FUNCTION__) + "() not implemented");
+    std::ifstream file(filePath);                                               //check if ifstream is used or something else
+    if (!file.is_open()) {
+        return;
+    }
+
+    int numEdges = 0;
+    int numVertices = 0;
+    int source = 0;
+    int destination = 0;
+    int weight = 0;
+    char color = 0;
+
+    file >> numVertices >> numEdges;    //reads the first two integers and stores
+    m_vertices.resize(numVertices);
+
+    //use num edges in case of directed
+    for (int i = 0; i < numEdges; i++) {
+        file >> source >> destination >> weight >> color;
+        file.ignore();  //ignore new line
+        enum color col;
+
+        if (color == 'R') {
+           col = RED;
+        } else if (color == 'B') {
+            col = BLUE;
+        } else if (color == 'G') {
+            col = GREEN;
+        } else {
+            col = NONE;
+        }
+
+        m_vertices[source].m_edges.push_back({weight, destination, col});    //only do one direction
+    }
+
+    file.close();
 }
--- src/max_height.cpp
+++ src/max_height.cpp
@@ -3,6 +3,11 @@
 using namespace cs251;
 
 int max_height::calculate(const graph& g) {
-	// TODO: Implement max height
-	throw std::logic_error("max_height::" + std::string(__FUNCTION__) + "() not implemented");
+	//essentially want to get the smallest largest height - supremum of heights that can go from any city to another
+    //  in the case of the example, 30 is the smallest largest height due to the cycle BGF such that to reach G 30 is
+    //  the largest smallest height
+
+    //use maximum spanning tree through Prim's algorithm
+
+    
 }
